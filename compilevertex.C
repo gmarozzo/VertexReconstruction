d2e0f0f9984e301651836ccd6856800be64fdf55
elisa-96 <78601875+elisa-96@users.noreply.github.com>
2021-02-08 18:44:28 +0100

    Update compilevertex.C

--- compilevertex.C
+++ compilevertex.C
@@ -1,5 +1,5 @@
 void compilemyclass (TString myopt="fast"){
 TString opt;
 if (myopt.Contains("force")){opt="kfg";} else {opt ="kg"; }
-gSystem->CompileMacro ("vertexh", opt.Data());
+gSystem->CompileMacro ("vertex.cpp", opt.Data());
 gSystem->CompileMacro ("vertexsimul.C", opt.Data()); }
