ae87a35c4cee48516126cb63c146b555d88a07d7
elisa-96 <78601875+elisa-96@users.noreply.github.com>
2021-02-09 14:18:55 +0100

    Update vertex.h

--- vertex.h
+++ vertex.h
@@ -8,26 +8,28 @@
 
 class vertex : public TObject {  
 public:                      
-   vertex(double sigmaz,  TString moltep="No", double zv=0,double xv=0., double yv=0.);   //vertice di default=origine
+   vertex(double sigmaz,  TString mult, double zv,double xv, double yv);   //vertice di default=origine
  ~vertex(); 
    
  
-  void prodotti(); 
+  void prod(); 
     double randgauss();
-  
-  
+   int multi ;//private
+  double *theta;//private
+  double *phi;//private
+   double x,y,z,sigma;
+      
   
   private:
-  int molteplicita ;
-  double *theta;
-  double *phi;
-  double x,y,z,sigma;
-      
+ 
+ 
   //void Setx();
   //void Sety();
   void Setz(); //cambio della coordinata z con distribuzione scelta     
-   void moltcost();
- void moltfunz();
+   void multconst();
+ void multfunc();
+ 
+ 
   ClassDef(vertex,1) 
 };          
 
